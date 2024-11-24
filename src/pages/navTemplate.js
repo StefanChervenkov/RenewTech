5506971d7e82c2a38d5913dda4e4aa3605e39e02
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2024-11-24 12:47:57 +0200

    Extracting the templates
    Extracted the templates into their own modules

---
+++ src/pages/navTemplate.js
@@ -0,0 +1,15 @@
+//  <div>
+// <a href="#">Solutions</a>
+// </div>
+
+// <!--Logged -in users-- >
+// <div class="user">
+// <a href="#">Add Solution</a>
+// <a href="#">Logout</a>
+// </div>
+
+// <!--Guest users-- >
+//     <div class="guest">
+//         <a href="#">Login</a>
+//         <a href="#">Register</a>
+//     </div> 
